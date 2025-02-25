39bab3d17d2091cd1eee804bb7a11920b8664d74
Patrick Schönfeld <p.schoenfeld@nexus-united.com>
2025-02-25 13:03:47 +0100

    prep deployment

--- .platform-scripts/deploy/pimcore-commands.sh
+++ .platform-scripts/deploy/pimcore-commands.sh
@@ -8,8 +8,8 @@ rm -f ./var/config/maintenance.php
 mkdir -p var/config
 echo '<?php return ["sessionId" => "command-line-dummy-session-id"];' >> var/config/maintenance.php
 
-php ./bin/console pimcore:deployment:classes-rebuild --create-classes --no-interaction --ignore-maintenance-mode
+#php ./bin/console pimcore:deployment:classes-rebuild --create-classes --no-interaction --ignore-maintenance-mode
 
-php ./bin/console doctrine:migrations:migrate --prefix=Pimcore\\Bundle\\CoreBundle --no-interaction --ignore-maintenance-mode
+#php ./bin/console doctrine:migrations:migrate --prefix=Pimcore\\Bundle\\CoreBundle --no-interaction --ignore-maintenance-mode
 
-php ./bin/console doctrine:migrations:migrate --no-interaction --ignore-maintenance-mode
+#php ./bin/console doctrine:migrations:migrate --no-interaction --ignore-maintenance-mode
