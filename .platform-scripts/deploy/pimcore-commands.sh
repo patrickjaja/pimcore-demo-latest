e3671676d92874343c1c9b9453f68d1e121d235b
Patrick Schönfeld <p.schoenfeld@nexus-united.com>
2025-02-25 15:55:25 +0100

    prep deployment

--- .platform-scripts/deploy/pimcore-commands.sh
+++ .platform-scripts/deploy/pimcore-commands.sh
@@ -19,5 +19,5 @@ php ./bin/console app:auto-map-json -i data/example/product_model.json -o localC
 php ./bin/console pimcore:definition:import:class localCache/class_StaticImportedProductType1_export.json  --ignore-maintenance-mode
 
 
-php ./bin/console app:auto-map-json -i data/example/product_model.json -o localCache/class_StaticImportedProductType1_export.json -p data/prompt/model_prompt.md
-php ./bin/console app:import-data-command -m localCache/class_StaticImportedProductType2_export.json -d localCache/data_StaticImportedProductType1_export.json
+php ./bin/console app:auto-map-json -i data/example/product_model.json -o localCache/class_StaticImportedProductType1_export.json -p data/prompt/model_prompt.md --ignore-maintenance-mode
+php ./bin/console app:import-data-command -m localCache/class_StaticImportedProductType2_export.json -d localCache/data_StaticImportedProductType1_export.json --ignore-maintenance-mode
