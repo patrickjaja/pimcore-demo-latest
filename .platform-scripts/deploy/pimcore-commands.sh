1aa5a615eba38c8779616fe1544021869ec58a41
Patrick Schönfeld <p.schoenfeld@nexus-united.com>
2025-02-25 15:01:45 +0100

    prep deployment

--- .platform-scripts/deploy/pimcore-commands.sh
+++ .platform-scripts/deploy/pimcore-commands.sh
@@ -14,5 +14,5 @@ echo '<?php return ["sessionId" => "command-line-dummy-session-id"];' >> var/con
 
 #php ./bin/console doctrine:migrations:migrate --no-interaction --ignore-maintenance-mode
 
-php ./bin/console app:auto-map-json -i data/example/product_model.json -o config/pimcore/class_StaticImportedProductType1_export.json -p data/prompt/model_prompt.md  --ignore-maintenance-mode
-php ./bin/console pimcore:definition:import:class config/pimcore/class_StaticImportedProductType1_export.json  --ignore-maintenance-mode
+php ./bin/console app:auto-map-json -i data/example/product_model.json -o var/class_StaticImportedProductType1_export.json -p data/prompt/model_prompt.md  --ignore-maintenance-mode
+php ./bin/console pimcore:definition:import:class var/class_StaticImportedProductType1_export.json  --ignore-maintenance-mode
