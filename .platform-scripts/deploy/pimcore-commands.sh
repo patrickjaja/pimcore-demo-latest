1c275626a583d697ca7ce7fc0775a59d579536a1
Patrick Schönfeld <p.schoenfeld@nexus-united.com>
2025-02-25 15:16:56 +0100

    prep deployment

--- .platform-scripts/deploy/pimcore-commands.sh
+++ .platform-scripts/deploy/pimcore-commands.sh
@@ -6,6 +6,7 @@ php ./bin/console cache:clear --no-interaction --ignore-maintenance-mode
 
 rm -f ./var/config/maintenance.php
 mkdir -p var/config
+#cp -R ./.platform/config/ var/config
 echo '<?php return ["sessionId" => "command-line-dummy-session-id"];' >> var/config/maintenance.php
 
 #php ./bin/console pimcore:deployment:classes-rebuild --create-classes --no-interaction --ignore-maintenance-mode
@@ -14,5 +15,5 @@ echo '<?php return ["sessionId" => "command-line-dummy-session-id"];' >> var/con
 
 #php ./bin/console doctrine:migrations:migrate --no-interaction --ignore-maintenance-mode
 
-php ./bin/console app:auto-map-json -i data/example/product_model.json -o localCache/class_StaticImportedProductType1_export.json -p data/prompt/model_prompt.md  --ignore-maintenance-mode
-php ./bin/console pimcore:definition:import:class localCache/class_StaticImportedProductType1_export.json  --ignore-maintenance-mode
+#php ./bin/console app:auto-map-json -i data/example/product_model.json -o localCache/class_StaticImportedProductType1_export.json -p data/prompt/model_prompt.md  --ignore-maintenance-mode
+#php ./bin/console pimcore:definition:import:class localCache/class_StaticImportedProductType1_export.json  --ignore-maintenance-mode
